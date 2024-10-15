fb75cfe92bde4a628e843261f5d7d5bac5f45e48
Migle <miglelazauskaite@gmail.com>
2024-10-15 22:04:14 +0300

    3 čia pamoka

---
+++ naujas.py
@@ -0,0 +1,30 @@
+x=10
+y=15
+print (x)
+print (y)
+suma=x+y
+print (suma)
+
+kint1=6
+kint2=13
+kint3=21
+suma=kint1+kint2+kint3
+print (suma)
+print ("suma: " + str(suma))
+
+print(str(kint1) + "+" + str(kint2) + "+" + str(kint3) + "=" + str(suma))
+
+sk1=16
+sk2=12
+ats1=((sk1)+5)
+ats2=((sk2)*2)
+print (str(sk1)+ " "+str(sk2)+ " "+str(ats1)+ " "+str(ats2))
+
+print (sk1*sk2)
+print (sk1**2)
+
+#math library
+import math
+print (math.sqrt(sk1))
+
+print (sk1**(1./2))
